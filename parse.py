593132db5102ff0035487a63d4eb9313656d2fa5
buttercm <buttercm@gmail.com>
2013-12-15 19:23:39 -0500

    first commit

---
+++ parse.py
@@ -0,0 +1,12 @@
+import csv
+
+MY_FILE="../data/sample_sfpd_incident_all.csv"
+
+def parse(raw_file, delimiter):
+    """Parses a raw CSV file to a JSON-line object."""
+
+    # Open CSV file
+
+
+
+    return parsed_data
